5afa63ff2d54836046f7b21030efc48df601b886
SyafnidesWulanS <80112538+SyafnidesWulanS@users.noreply.github.com>
2021-05-28 23:22:17 +0800

    Add files via upload

---
+++ 20523099.R
@@ -0,0 +1,12 @@
+#Exercise1
+yac_expr("D(x) 2*x^5")
+yac_expr("D(x) x^2+4")
+yac_expr("D(x) x^5-6*x^7")
+
+#Exercise2
+yac_expr("D(x) (x^6-2)*(Sin(x)-4)")
+yac_expr("D(x) (sin(x)-4)*(x^6-2)")
+yac_expr("D(x) (x^6-2)/(Sin(x)-4)")
+yac_expr("D(x) (sin(x)-4)/(x^6-2)")
+yac_expr("D(x) (sin(x)-4)^6-2")
+yac_expr("D(x) sin(x^6-2)-4")
